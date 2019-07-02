b7d702530afcaad3e21259f669f33b1b605dadf0
narci <narci.ltc@toceansoft.com>
2019-07-02 17:08:25 +0800

    update

--- template/init/instance.ts
+++ template/init/instance.ts
@@ -2,19 +2,20 @@
  * @Description: 针对每个项目，提供一个instance对象（主要解决一个前端可能跟多个后台系统交互的场景，如现在流行的微服务）
  * @Author: Narci.Lee(narci2010@qq.com)
  * @Date: 2019-06-11 10:39:04
- * @LastEditTime: 2019-07-02 10:23:07
+ * @LastEditTime: 2019-07-02 17:07:37
  * @LastEditors: Please set LastEditors
  */
 
 import { createAPI } from '../util'
 import config from '../config'
 
-const baseUrl = {
+const baseUrlObject: { [s: string]: string } = {
   mock:
     '{{$$.joinUrl(config.host, "mock", data.project._id, data.project.url)}}',
   dev: '',
   pre: '',
   prod: ''
-}[config.env || 'mock']
+}
+const baseUrl = baseUrlObject[config.env || 'mock']
 
 export default createAPI(baseUrl)

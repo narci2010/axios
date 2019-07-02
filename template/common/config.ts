d7e384210b2676157072311230d7e846f97b57d3
narci <narci.ltc@toceansoft.com>
2019-07-02 17:12:56 +0800

    update

--- template/common/config.ts
+++ template/common/config.ts
@@ -2,10 +2,10 @@
  * @Description: 配置环境文件
  * @Author: Narci.Lee(narci2010@qq.com)
  * @Date: 2019-06-11 10:39:04
- * @LastEditTime: 2019-07-02 10:13:59
+ * @LastEditTime: 2019-07-02 17:12:48
  * @LastEditors: Please set LastEditors
  */
 
 export default {
-  env: 'mock' // 取值返回mock,dev,pre,prod
+  env: 'mock' // 取值范围mock,dev,pre,prod
 }
